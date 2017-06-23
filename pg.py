a849ea86d20bba8331a375b86774277786c20f2a
Abhishek Bhatia <abhigenie92@gmail.com>
2017-06-23 19:43:11 -0400

    Added cmdline arguments to enable/disable game rendering, with few additional comments to help for understanding code.

--- pg.py
+++ pg.py
@@ -1,10 +1,9 @@
-import gym
+import gym, pdb, argparse, sys, distutils.util
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Reshape, Flatten
 from keras.optimizers import Adam
 from keras.layers.convolutional import Convolution2D
-
 
 class PGAgent:
     def __init__(self, state_size, action_size):
@@ -34,16 +33,24 @@ class PGAgent:
 
     def remember(self, state, action, prob, reward):
         y = np.zeros([self.action_size])
+        # one-hot vector for the action taken
         y[action] = 1
+        # compute the gradient with respect to probabilities in 
+        #   forward prop. and action taken
         self.gradients.append(np.array(y).astype('float32') - prob)
+        # append current states and rewards
         self.states.append(state)
         self.rewards.append(reward)
 
     def act(self, state):
+        # reshape from 6400 to (1, 6400)
         state = state.reshape([1, state.shape[0]])
+        # predict the actions probabilities with a forward propagation
         aprob = self.model.predict(state, batch_size=1).flatten()
         self.probs.append(aprob)
+        # normalize the probabilities (seems not required though)
         prob = aprob / np.sum(aprob)
+        # select an action with respect to the probabilities obtained
         action = np.random.choice(self.action_size, 1, p=prob)[0]
         return action, prob
 
@@ -60,7 +67,9 @@ class PGAgent:
     def train(self):
         gradients = np.vstack(self.gradients)
         rewards = np.vstack(self.rewards)
+        # discount the rewards
         rewards = self.discount_rewards(rewards)
+        # normalize the rewards
         rewards = rewards / np.std(rewards - np.mean(rewards))
         gradients *= rewards
         X = np.squeeze(np.vstack([self.states]))
@@ -83,6 +92,9 @@ def preprocess(I):
     return I.astype(np.float).ravel()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--debug",default=False,type=distutils.util.strtobool)
+    args,_ = parser.parse_known_args([arg for arg in sys.argv[1:] if arg not in ('-h', '--help')])
     env = gym.make("Pong-v0")
     state = env.reset()
     prev_x = None
@@ -94,7 +106,9 @@ if __name__ == "__main__":
     agent = PGAgent(state_size, action_size)
     agent.load('pong.h5')
     while True:
-        env.render()
+    	if args.debug:
+    		print args.debug
+        	env.render()
 
         cur_x = preprocess(state)
         x = cur_x - prev_x if prev_x is not None else np.zeros(state_size)
@@ -104,7 +118,6 @@ if __name__ == "__main__":
         state, reward, done, info = env.step(action)
         score += reward
         agent.remember(x, action, prob, reward)
-
         if done:
             episode += 1
             agent.train()
